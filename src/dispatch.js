73c53b69883f77d106ef90335daca85f1c56f423
Giacomo Guerci <giacomo.guerci14@imperial.ac.uk>
2018-05-18 03:41:45 +0100

    Employ env variables

--- src/dispatch.js
+++ src/dispatch.js
@@ -2,24 +2,6 @@ import "isomorphic-fetch"
 import "babel-polyfill"
 
 
-let experimentID = "";
-let versionID = "";
-
-fetch('/hype/config', {
-  method: 'POST',
-  headers: {
-    "Content-type": "application/json"
-  },
-  body: JSON.stringify({
-    "url":document.URL
-  })
-}).then((response) => {
-  return response.json();
-}).then((data) => {
-  experimentID = data["experiment-id"];
-  versionID = data["version-id"];
-});
-
 export default async function dispatch(data) {
   await fetch('/hype/metrics', {
     method: 'POST',
@@ -27,8 +9,8 @@ export default async function dispatch(data) {
       "Content-type": "application/json"
     },
     body: JSON.stringify({
-      "experiment-id":experimentID,
-      "version-id":versionID,
+      "experiment-id": process.env.REACT_APP_HYPE_EXP_ID || "",
+      "version-id": process.env.REACT_APP_HYPE_EXP_VERS || "",
       "data":data
     })
   }).then((response) => {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 'use strict';
 
 import EventMetric from "./metrics/EventMetric";
+import dispatch from "./dispatch";
 
 
 export function logEvent(event) {
   return new EventMetric(event);
+}
+
+export function recordClick(event) {
+  let eventInfo = {
+    type: "click",
+    elem: event.target.id,
+  }
+  dispatch(eventInfo);
+}
+
+export function recordPageView(document) {
+  let eventInfo = {
+    type: "pageview",
+    elem: document,
+  }
+  dispatch(eventInfo);
 }
