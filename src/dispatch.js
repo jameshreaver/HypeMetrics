5beb3db5859dccce713076bf731f29f0ac491867
Giacomo Guerci <giacomo.guerci14@imperial.ac.uk>
2018-05-23 14:41:31 +0100

    Update post URL

--- src/dispatch.js
+++ src/dispatch.js
@@ -3,19 +3,17 @@ import "babel-polyfill"
 
 
 export default async function dispatch(data) {
-  await fetch('/hype/metrics', {
+  fetch('http://hype-metrics', {
     method: 'POST',
     headers: {
       "Content-type": "application/json"
     },
     body: JSON.stringify({
-      "experiment-id": process.env.REACT_APP_HYPE_EXP_ID || "",
-      "version-id": process.env.REACT_APP_HYPE_EXP_VERS || "",
-      "data":data
+      "eid": process.env.REACT_APP_HYPE_EXP_ID,
+      "vid": process.env.REACT_APP_HYPE_EXP_VERS,
+      "data": data
     })
-  }).then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data);
+  }).catch((err) => {
+    console.log(err)
   });
 }
